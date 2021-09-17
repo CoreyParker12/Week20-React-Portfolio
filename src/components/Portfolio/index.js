b82ef6cc370d47a233d660aa0d16c09d35dc1c8e
Corey Parker <coreyparker012@gmail.com>
2021-09-16 22:07:46 -0400

    added portfolio images

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,34 +7,41 @@ function Portfolio() {
   // Replace links with deployed projects and GitHub repos
   const [projects] = useState([
     {
-      name: 'getHAPI-logo',
+      name: 'getHAPI',
       description: 'Get HAPI',
       link: "https://coreyparker12.github.io/Project-1-getHAPI/",
       repo: "https://github.com/CoreyParker12/Project-1-getHAPI"
     },
     {
-      name: 'led-wall',
+      name: 'Stinky-Bass',
       description: 'Stinky Bass',
       link: "http://stinkybass.com/",
     },
     {
-      name: 'run-buddy',
-      description: 'HTML/CSS',
+      name: 'Weather-API',
+      description: 'Weather API',
       link: "https://coreyparker12.github.io/Week6-WeatherAPI-CP/",
       repo: "https://github.com/CoreyParker12/Week6-WeatherAPI-CP"
     },
     {
-      name: 'led-wall',
+      name: 'Toolin-Around',
       description: 'Toolin\' Around',
       link: "https://toolin-around.herokuapp.com/",
       repo: "https://github.com/CoreyParker12/Toolin-Around"
     },
     {
-      name: 'calculator',
+      name: 'Simple-Tech',
       description: 'Simple Tech',
       link: "https://tech-blog-coreyparker.herokuapp.com/",
       repo: "https://github.com/CoreyParker12/Week14-Tech-Blog"
     },
+    {
+      name: 'Note-Taker',
+      description: 'Note Taker',
+      link: "https://note-taker-coreyparker.herokuapp.com/",
+      repo: "https://github.com/CoreyParker12/Week11-Note-Taker"
+    },
+    
   ]);
 
   return (
