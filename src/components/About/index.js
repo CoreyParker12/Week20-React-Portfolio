f8f777914a5a07b37ede6817717047b6a9cc20cd
Corey Parker <coreyparker012@gmail.com>
2021-09-16 20:39:04 -0400

    fonts and homepage

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,13 +5,10 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        <img src="../../../public/Corey.JPG" alt="Corey Parker"></img>
         </div>
         <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
-        </p>
-        <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
+        Welcome to my portfolio site. My name is Corey Parker, and I'm an electrical engineer in Atlanta, Georgia. I have experience in HTML, CSS, JS, and React. I am still learning and hope to continue working on larger projects in the future. Please enjoy some of my more recent work, and feel free to reach out if you are interested in any of my projects. Links to my Github and LinkedIn are in the footer of my site.
         </p>
       </div>
     </section>
